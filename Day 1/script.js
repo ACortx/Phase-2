c568b222dde9d9d50b63a74b09b422d210eb259f
Ahmad Alobaid <133094832+AhmadAlobaidZain@users.noreply.github.com>
2023-12-03 17:42:08 +0300

    First Button Example Done

---
+++ Day 1/script.js
@@ -0,0 +1,9 @@
+// java script initial example
+
+//selecting button by id
+const button = document.getElementById("myButton");
+
+//add event listener to button, for when clicked
+button.addEventListener("click", function () {
+  alert("Here's what's inside the button itself");
+});
